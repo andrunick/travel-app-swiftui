b30430e5d732fa3ed04a32d61f6228f7e6a384bb
andrunick <andrunickson24@gmail.com>
2021-08-31 23:55:52 -0400

    story advancing on tap updated

---
+++ TravelingApp/RatingView.swift
@@ -0,0 +1,42 @@
+//
+//  RatingView.swift
+//  TravelingApp
+//
+//  Created by Usman Mukhtar on 15/08/2020.
+//
+
+import SwiftUI
+
+struct RatingView: View {
+    @Binding var rating: Int
+    var label = ""
+    var maximumRating = 5
+    var offImage: Image?
+    var onImage = Image(systemName: "star.fill")
+    var offColor = Color.gray
+    var onColor = Color.orange
+    
+    var body: some View {
+        HStack {
+            if label.isEmpty == false {
+                Text(label)
+            }
+            
+            ForEach(1..<maximumRating + 1) { number in
+                self.image(for:number)
+                    .foregroundColor(number > self.rating ? self.offColor : self.onColor)
+                    .onTapGesture {
+                        self.rating = number
+                    }
+            }
+        }
+    }
+    
+    func image(for number: Int) -> Image {
+        if number > rating {
+            return offImage ?? onImage
+        } else {
+            return onImage
+        }
+    }
+}
